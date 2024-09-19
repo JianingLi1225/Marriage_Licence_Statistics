00dd7d88535c8813d3066a14ac5963a343650fe4
JianingLi1225 <lijianing.li@mail.utoronto.ca>
2024-09-19 17:50:15 -0400

    change code

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,22 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Sanity check of the data
+# Author: Rohan Alexander
+# Date: 19 September 2024
+# Contact: rohan.alexander@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Need to have simulated data
+# Any other information needed? None.
 
 
 #### Workspace setup ####
 library(tidyverse)
-library(rstanarm)
-
-#### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
-
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
 
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
-)
+#### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
 
+# Test for negative numbers
+data$number_of_marriage |> min() <= 0
 
+# Test for NAs
+all(is.na(data$number_of_marriage))
